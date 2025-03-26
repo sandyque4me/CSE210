0c172b19d6c98b35bbf4dd8cd015a9f7fe166c6e
Sandra Ezeh <sandybest4lifes@gmail.com>
2025-03-26 10:52:17 +0100

    finished week 02

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -86,8 +86,16 @@ namespace ScriptureHider
     {
         static void Main(string[] args)
         {
-            var scripture = new Scripture("Proverbs", "3", "5-6",
-                "Trust in the Lord with all your heart and lean not on your own understanding");
+            var scriptureLibrary = new List<Scripture>
+            {
+                new Scripture("Proverbs", "3", "5-6", "Trust in the Lord with all your heart and lean not on your own understanding"),
+                new Scripture("John", "3", "16", "For God so loved the world that he gave his one and only Son that whoever believes in him shall not perish but have eternal life"),
+                new Scripture("Psalm", "23", "1", "The Lord is my shepherd I shall not want"),
+                new Scripture("Philippians", "4", "13", "I can do all things through Christ who strengthens me")
+            };
+
+            var random = new Random();
+            var scripture = scriptureLibrary[random.Next(scriptureLibrary.Count)];
 
             while (true)
             {
